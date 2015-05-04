c2fa0507cbd4a29e51f7c02a11b7a150d73f6bbd
jaimebarez <jaimebarez@m-14-killer-w>
2015-05-04 22:58:58 +0200

    First commit of the project

---
+++ ResourceSchedulerNetBeansProject/test/resourcescheduler/ResourceSchedulerTest.java
@@ -0,0 +1,36 @@
+package resourcescheduler;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jaime Bárez Lobato
+ */
+public class ResourceSchedulerTest {
+    
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    
+   
+    
+}
