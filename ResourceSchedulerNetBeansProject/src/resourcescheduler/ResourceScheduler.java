89e132301ac3a8ad643c1173e0a233a608bc9bf6
jaimebarez <jaimebarez@m-14-killer-w>
2015-05-04 23:06:06 +0200

    We write some code that causes the test to pass in an inelegant way. When we run the test, it passes.

---
+++ ResourceSchedulerNetBeansProject/src/resourcescheduler/ResourceScheduler.java
@@ -0,0 +1,23 @@
+package resourcescheduler;
+
+/**
+ *
+ * @author Jaime Bárez Lobato
+ */
+public class ResourceScheduler {
+
+    private int resourcesQuantity;
+
+    public ResourceScheduler() {
+        this.resourcesQuantity = 2;
+    }
+
+    public int getResourcesQuantity() {
+        return resourcesQuantity;
+    }
+
+    public void setResourcesQuantity(int resourcesQuantity) {
+        
+    }
+
+}
