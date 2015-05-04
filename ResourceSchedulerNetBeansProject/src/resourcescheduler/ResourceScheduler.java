0974d87249bd68c7992f571e61c1036b2924f021
jaimebarez <jaimebarez@m-14-killer-w>
2015-05-05 00:33:37 +0200

    Inelegant code to pass the tests. Thank to TDD and a previous test that generated high numbers of available Gateways, I realized my inelegant code gas TOO INELEGANT.
    I'm remembering again the benefits of TDD.
    Cons: Slowlier developing.
    
    Sleeping time. I'll continue tomorrow

--- ResourceSchedulerNetBeansProject/src/resourcescheduler/ResourceScheduler.java
+++ ResourceSchedulerNetBeansProject/src/resourcescheduler/ResourceScheduler.java
@@ -1,5 +1,6 @@
 package resourcescheduler;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 import resourcescheduler.model.gateway.Gateway;
@@ -11,42 +12,55 @@ import resourcescheduler.model.message.Message;
  * @author Jaime Bárez Lobato
  */
 public class ResourceScheduler {
-
+    
     private int resourcesQuantity;
     private final Queue<Message> unsentMessageQueue;
     private final Queue<Gateway> unusedResources;
     private final GatewayAbstractFactory gatewayFactory;
-
+    
     public ResourceScheduler(GatewayAbstractFactory gatewayFactory) {
         this.gatewayFactory = gatewayFactory;
-
+        
         this.resourcesQuantity = 0;
         this.unsentMessageQueue = new LinkedList<>();
-        unusedResources = new LinkedList<>();
+        this.unusedResources = new LinkedList<>();
     }
-
+    
     public int getResourcesQuantity() {
         return resourcesQuantity;
     }
-
+    
     public void setResourcesQuantity(int newResourcesQuantity) {
         final int oldResourcesQuantity = this.resourcesQuantity;
         this.resourcesQuantity = newResourcesQuantity;
-
-        int numberOfNewResource = Math.max(0, newResourcesQuantity - oldResourcesQuantity);
-
-        for (Message unsentMessageQueue1 : unsentMessageQueue) {
-
+        
+        final int numberOfNewResources = Math.max(0, newResourcesQuantity - oldResourcesQuantity);
+        
+        for (int i = 0; i < numberOfNewResources; i++) {
+            unusedResources.offer(gatewayFactory.createGateway());
         }
-
+        
+        Iterator<Message> unsentMsgsIt = unsentMessageQueue.iterator();
+        Iterator<Gateway> unusedResourcesIt = unusedResources.iterator();
+        
+        int numResourcesCouldProcess = numberOfNewResources;
+        
+        while (numResourcesCouldProcess >= 0 && unsentMsgsIt.hasNext() && unusedResourcesIt.hasNext()) {
+            Message nextMessageToSend = unsentMsgsIt.next();
+            Gateway nextNewGateway = unusedResourcesIt.next();
+            nextNewGateway.send(nextMessageToSend);
+            unusedResourcesIt.remove();
+            numResourcesCouldProcess--;
+        }
+        
     }
-
+    
     public void reveiveMessage(Message message) {
         this.unsentMessageQueue.offer(message);
     }
-
+    
     public int getQueuedMessagesCount() {
         return this.unsentMessageQueue.size();
     }
-
+    
 }
